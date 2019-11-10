2302d8773a2658c8b12edcb6c0ddf0e55ad35d5d
Mihir Shah <mihir-ms>
2019-11-10 13:42:09 +0530

    html css pages

--- AIP/urls.py
+++ AIP/urls.py
@@ -1,12 +1,21 @@
 from django.contrib import admin
 from django.urls import path,include,re_path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     re_path(r'^$', views.index , name='index'),
+    re_path(r'^nav/', views.nav , name='nav'),
+    re_path(r'^login/', views.login , name='signin'),
+    re_path(r'^candidate signup/', views.candidate_signup , name='signup'),
+    re_path(r'^recruiter signup/', views.recruiter_signup , name='signup'),
     re_path(r'^admin/', admin.site.urls),
     re_path(r'^accounts/', include('accounts.urls')),
     re_path(r'^recruiter/', include('recruiter.urls')),
     re_path(r'^maintainer/', include('maintainer.urls')),
     re_path(r'^candidate/', include('candidate.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- AIP/views.py
+++ AIP/views.py
@@ -6,3 +6,17 @@ from django.http import HttpResponse
 def index(request):
     return render(request,'AIP/index.html')
 
+def nav(request):
+    return render(request,'AIP/nav.html')
+
+def login(request):
+    return render(request,'AIP/signin.html')
+
+
+def candidate_signup(request):
+    return render(request,'candidate/candidate signup.html')
+
+
+def recruiter_signup(request):
+    return render(request,'recruiter/recruiter signup.html')
+
